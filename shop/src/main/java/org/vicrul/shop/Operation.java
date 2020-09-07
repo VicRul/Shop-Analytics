1fbbd60e1c080aca3fb277308ebf48e65856ce19
victor <victor@victor-PC>
2020-09-07 10:16:03 +0700

    Готов метод сбора статистики

--- shop/src/main/java/org/vicrul/shop/Operation.java
+++ shop/src/main/java/org/vicrul/shop/Operation.java
@@ -1,10 +1,13 @@
 package org.vicrul.shop;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.Iterator;
 import java.util.List;
 
@@ -13,10 +16,10 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.vicrul.shop.model.Customer;
+import org.vicrul.shop.model.PeriodData;
+import org.vicrul.shop.model.Product;
 import org.vicrul.shop.service.CustomerService;
 import org.vicrul.shop.service.CustomerServiceImpl;
-
-import lombok.AllArgsConstructor;
 
 public class Operation {
 
@@ -28,8 +31,10 @@ public class Operation {
 		Iterator element = creterias.iterator();
 		JSONObject result = new JSONObject();
 		result.put("type", "search");
+		
 		while (element.hasNext()) {
 			JSONObject innerObj = (JSONObject) element.next();
+			
 			if (innerObj.containsKey("surname")) {
 				String surname = (String) innerObj.get("surname");
 				if (surname.isEmpty()) {
@@ -89,6 +94,59 @@ public class Operation {
 		writeInFile(result, outputFilePath);
 	}
 
+	public void stat(String inputFilePath, String outputFilePath) {
+
+		JSONObject jsonObject = readFromFile(inputFilePath, outputFilePath);
+		JSONObject result = new JSONObject();
+		result.put("type", "stat");
+
+		String startDate = (String) jsonObject.get("startDate");
+		String endDate = (String) jsonObject.get("endDate");
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-M-dd");
+		LocalDate date1 = LocalDate.parse(startDate, dtf);
+		LocalDate date2 = LocalDate.parse(endDate, dtf);
+
+		int countDays = 0;
+		for (LocalDate date = date1; date.isBefore(date2); date = date.plusDays(1)) {
+			Calendar gc = GregorianCalendar.from(date.atStartOfDay(ZoneId.systemDefault()));
+			int day = gc.get(Calendar.DAY_OF_WEEK);
+			if (day > 1 && day < 7) {
+				countDays++;
+			}
+		}
+		result.put("totalDays", countDays);
+
+		customerService.createView(startDate, endDate);
+		List<Customer> customers = customerService.getCustomersInPeriod();
+		JSONArray customersArray = new JSONArray();
+
+		for (Customer customer : customers) {
+			JSONObject jsonCustomers = new JSONObject();
+			jsonCustomers.put("name", customer.getLastName().concat(" ").concat(customer.getFirstName()));
+			List<Product> purchases = customerService.getCustomerPurchses(customer.getLastName(), customer.getFirstName());
+			jsonCustomers.put("purchases", purchases);
+			
+			int customerTotalExpenses = 0;
+			for (Product product : purchases) {
+				customerTotalExpenses += product.getPrice();
+			}
+			jsonCustomers.put("totalExpenses", customerTotalExpenses);
+			customersArray.add(jsonCustomers);
+		}
+		result.put("customers", customersArray);
+		
+		List<PeriodData> allData = customerService.getAllData();
+		int totalFullExpenses = 0;
+		for (PeriodData periodData : allData) {
+			totalFullExpenses += periodData.getPrice();
+		}
+		int avgExpenses = totalFullExpenses / allData.size();
+		result.put("totalExpenses", totalFullExpenses);
+		result.put("avgExpenses", avgExpenses);
+		
+		writeInFile(result, outputFilePath);
+	}
+
 	private void errorOperation(String message, String outputFilePath) {
 		JSONObject object = new JSONObject();
 		object.put("type", "error");
@@ -115,7 +173,7 @@ public class Operation {
 			}
 		}
 	}
-	
+
 	private JSONObject readFromFile(String inputFilePath, String outputFilePath) {
 		FileReader reader = null;
 		JSONObject jsonObject = null;
--- shop/src/main/java/org/vicrul/shop/Run.java
+++ shop/src/main/java/org/vicrul/shop/Run.java
@@ -17,71 +17,27 @@ public class Run {
 
 	public static void main(String[] args) {
 		
-		String command = args[0];
-		String input = args[1];
-		String output = args[2];
-		
-		System.out.println(command + " " + input + " " + output);
-
-		CustomerService customerService = new CustomerServiceImpl();
-		List<Customer> customers1 = customerService.findBySurname("Иванов");
-		List<Customer> customers2 = customerService.findWhoBoughtThisProduct("Минералка", 0);
-		List<Customer> customers3 = customerService.intervalCost(100, 500);
-		List<Customer> customers4 = customerService.findPassiveCustomers(4);
-		// customerService.createView("2020-05-20", "2020-05-24");
-		// customerService.dropView();
-		List<Customer> customers5 = customerService.getCustomersInPeriod();
-
-		getList(customers1);
-		System.out.println("==============================================================");
-		getList(customers2);
-		System.out.println("==============================================================");
-		getList(customers3);
-		System.out.println("==============================================================");
-		getList(customers4);
-		System.out.println("==============================================================");
-		getList(customers5);
-//=============================================================================================================
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-M-dd");
-		String dateInString1 = "2020-05-20";
-		String dateInString2 = "2020-05-24";
-		LocalDate date1 = LocalDate.parse(dateInString1, dtf);
-		LocalDate date2 = LocalDate.parse(dateInString2, dtf);
-
-		int countDays = 0;
-		for (LocalDate date = date1; date.isBefore(date2); date = date.plusDays(1)) {
-			Calendar gc = GregorianCalendar.from(date.atStartOfDay(ZoneId.systemDefault()));
-			int day = gc.get(Calendar.DAY_OF_WEEK);
-			if(day > 1 && day < 7) {
-				countDays ++;
-			}
-	    }
-		System.out.println("Ответ = " + countDays);
-//=================================================================================================================	
-		List<Product> purchases = customerService.getCustomerPurchses("Иванов", "Иван");
-		
-		long totalExpenses = 0;
-		for (Product product : purchases) {
-			System.out.println(product.toString());
-			totalExpenses += product.getPrice();
+		String inputCommand = "", inputFilePath = "", outputFilePath = "";
+		try {
+			inputCommand = args[0];
+			inputFilePath = args[1];
+			outputFilePath = args[2];
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Указаны не все входные параметры при запуске.\n "
+					+ "Необходимо ввести операцию [search или stat], файл с критериями [*.json], файл для записи [*.json]");
 		}
-		System.out.println(totalExpenses);
 
-//===================================================================================================================			
-		List<PeriodData> allData = customerService.getAllData();
-		
-		long totalFullExpenses = 0;
-		for (PeriodData periodData : allData) {
-			totalFullExpenses += periodData.getPrice();
-		}
-		System.out.println("Общая сумма = " + totalFullExpenses);
-		System.out.println("Средняя общая сумма = " + (double) totalFullExpenses/allData.size());
-	}
+		switch (inputCommand) {
+		case "search":
+			new Operation().search(inputFilePath, outputFilePath);
+			break;
 
-	public static List<Customer> getList(List<Customer> customers) {
-		for (Customer customer : customers) {
-			System.out.println(customer.toString());
+		case "stat":
+			new Operation().stat(inputFilePath, outputFilePath);
+			break;
+
+		default:
+			break;
 		}
-		return customers;
 	}
 }
